f1007f2d3a3928575ed28da281e5860fcc26ed63
siddharth <siddharthrandive000@gmail.com>
2021-11-02 00:22:19 +0530

    ML Project

--- Advance_Python/Matplotlib/SimplePlot.py
+++ Advance_Python/Matplotlib/SimplePlot.py
@@ -1,5 +1,13 @@
 # Simple Plot
 
 import matplotlib.pyplot as plt
-plt.plot([1,2,3,4],[5,6,7,8])
+
+plt.plot([0, 0], [-1, 1], linewidth=5, zorder=3)
+plt.plot([-1, 1], [-1, 1], linewidth=5, zorder=2)
+plt.plot([-1, 1], [1, -1], linewidth=5, zorder=1)
+
+plt.xlabel("x label", color="r", size=20, labelpad=10)
+plt.ylabel("y label")
+plt.title("Graph Plot")
+
 plt.show()
